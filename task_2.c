0cddef1d6da573c592291a8cece9e455980ba5a8
Andrey Khokhlov <kron-k@ya.ru>
2024-05-19 18:10:51 +0300

    Added corrections

--- task_2.c
+++ task_2.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+// int main(void) {
+//     printf("    Let's\n");
+//     printf("       go\n");
+//     printf("        to walk");
+
+//     return 0;
+// }
+
+// OR
 int main(void) {
-    printf("    Let's\n");
-    printf("       go\n");
-    printf("        to walk");
+    printf("%9s", "Let's\n");
+    printf("%9s", "go\n");
+    printf("%14s", "to walk");
 
     return 0;
 }
